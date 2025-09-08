924473d5845832fa20088ccd0a78f0abae59b0de
Esbjörn <aesirbear@gmail.com>
2025-09-08 15:43:32 +0200

    Add project files.

---
+++ MyfirstProgram1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Esbjörn Holmerin Nord, Fullstack .Net 2025
+
+// Create an int with a number
+int number = 5;
+
+// Checks if the number is bigger than 10 and respons accordingly
+if (number > 10)
+{
+    Console.WriteLine("Talet är stort!");
+}
+else
+{
+    Console.WriteLine("Oj. Lågt tal!");
+}
+
+string input = "x";
+
+// Takes reads what a user inputs and stores it in a string variable.
+// The do while loop makes sure that the user ctually inputs a something to avoid a null value
+do
+{
+    Console.WriteLine("Vad heter du?");
+    input = Console.ReadLine();
+
+    if (input == null || input.Length < 1)
+    {
+        Console.WriteLine("Du behöver inte använda ditt riktiga namn, men skriv något iaf.");
+    }
+}
+while (input == null || input.Length < 1);
+
+// Greets the user with their input name
+Console.WriteLine("Hej, " + input);
+
+
+// Prints every number from 0 to the arbitrarily decided number at the start of the program
+for (int i = 0; i <= number; i++)
+{
+    Console.WriteLine(i);
+}
